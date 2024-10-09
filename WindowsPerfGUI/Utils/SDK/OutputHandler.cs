05d5e907c87fd803fbd8a75d4b4c95638b6cc4c0
nader.zouaoui <nader.zouaoui@edonec.com>
2024-10-09 11:57:13 +0100

    doc: annotate OutputHandler class

--- WindowsPerfGUI/Utils/SDK/OutputHandler.cs
+++ WindowsPerfGUI/Utils/SDK/OutputHandler.cs
@@ -34,9 +34,44 @@ namespace WindowsPerfGUI.Utils.SDK
 {
     public class OutputHandler
     {
+        /// <summary>
+        /// OutputHandler is a class that provides a simple way handle `DataReceivedEventHandler` and plug to the `StdOutput` / `StdError` of process.
+        ///     <example>
+        ///         <code>
+        ///       var StdOutput = new OutputHandler();
+        ///       var StdError = new OutputHandler();
+        ///       var process = new Process();
+        ///       {
+        ///         StartInfo = {
+        ///           FileName = "ping.exe",
+        ///           RedirectStandardOutput = true,
+        ///           RedirectStandardError = true,
+        ///           RedirectStandardInput = true,
+        ///           Arguments = "-t 127.0.0.1"
+        ///         }
+        ///       }
+        ///       process.OutputDataReceived += new DataReceivedEventHandler(StdOutput.OutputhHandler);
+        ///       process.ErrorDataReceived += new DataReceivedEventHandler(StdError.OutputhHandler);
+        ///       process.Start();
+        ///       
+        ///       // if you want to react to all the incoming data
+        ///       StdOutput.OutputCb = (string data) => {
+        ///         Debug.WriteLine(data);
+        ///       };
+        ///       StdError.OutputCb = (string data) => {
+        ///         Debug.WriteLine(data);
+        ///       };
+        ///         </code>
+        ///     </example>
+        /// </summary>
         public OutputHandler() { }
-
+        /// <summary>
+        /// A `List<string>` that contains the output of the process.
+        /// </summary>
         public List<string> Output = new();
+        /// <summary>
+        /// This is callback function that is executed whenever the process outputs something.
+        /// </summary>
         public Action<string> OutputCb { get; set; }
 
         public void ClearOutput()
